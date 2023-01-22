718199c126961ce2fe18dae80da60088d1b60fa1
František Poboček <pobocekfrantisek@gmail.com>
2023-01-22 20:24:49 +0100

    quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -30,12 +30,19 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
+
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(s) => output.push(format!("{}{}", string, "bar".repeat(*s))),
+            }
         }
+
         output
     }
 }
@@ -43,8 +50,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use my_module::transformer;
 
     #[test]
     fn it_works() {
