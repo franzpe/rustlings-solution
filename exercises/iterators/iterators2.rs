1b3334a239afd50ae58a0d079fca6270bcc98bbb
František Poboček <pobocekfrantisek@gmail.com>
2023-01-25 22:57:43 +0100

    iterators

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -3,16 +3,15 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
+
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_string().to_uppercase() + c.as_str(),
     }
 }
 
@@ -49,12 +48,17 @@ mod tests {
     #[test]
     fn test_iterate_string_vec() {
         let words = vec!["hello", "world"];
-        assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
+        let capitalized_words: Vec<String> = words.iter().map(|w| capitalize_first(w)).collect();
+        assert_eq!(capitalized_words, ["Hello", "World"]);
     }
 
     #[test]
     fn test_iterate_into_string() {
         let words = vec!["hello", " ", "world"];
-        assert_eq!(capitalize_words_string(&words), "Hello World");
+        let capitalized_words = words
+            .iter()
+            .map(|w| capitalize_first(w))
+            .collect::<String>();
+        assert_eq!(capitalized_words, String::from("Hello World"));
     }
 }
